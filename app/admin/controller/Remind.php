a19312b9ec948cbc3f5f9340632982e5ef433e57
zufangke <linchuantuo2016php@163.com>
2018-12-03 17:44:39 +0800

    模版页面上传

---
+++ app/admin/controller/Remind.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/12/3
+ * Time: 16:44
+ */
+namespace app\admin\controller;
+use think\Controller;
+class Remind extends Controller
+{
+    public function index()
+    {
+        echo 'Remind';
+    }
+}
