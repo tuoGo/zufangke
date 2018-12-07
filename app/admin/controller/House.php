ff03b4c521baf4b8d6e386f71b81286115f8198b
zufangke <602029937@qq.com>
2018-12-07 13:46:47 +0800

    house增加编辑模块

--- app/admin/controller/House.php
+++ app/admin/controller/House.php
@@ -15,4 +15,38 @@ class House extends Controller
     {
         return $this->fetch('house/house_list');
     }
+
+    /**
+     * 编辑(添加或修改)
+     */
+    public function edit(Request $request){
+        try{
+
+            if($request->isPost()){//若为post则添加
+                $result = input('post');
+                $data = array(
+                    'address'   => $result['address'],
+                    'area'      => $result['area'],
+                    'doormodel' => $result['doormodel'],
+                    'status'    => $result['status']
+                );
+                if($result['id'] == null){
+                    //添加
+                    //$this->add($data);
+                }else{
+                    //修改
+                    //$this->update($id,$data);
+                }
+
+            }else{//修改回显
+                $id = input('get')['id'];
+                //$this->select($id);
+            }
+
+        }catch (Exception $e){
+            $this->error('编辑错误');
+        }
+
+
+    }
 }
