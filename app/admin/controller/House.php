b2388956086de561083ea2129ff4b78b51428808
zufangke <602029937@qq.com>
2018-12-10 16:07:50 +0800

    房源模块更新

--- app/admin/controller/House.php
+++ app/admin/controller/House.php
@@ -8,6 +8,8 @@
 namespace app\admin\controller;
 use app\common\Common;
 use think\Controller;
+use think\Exception;
+use think\Request;
 
 class House extends Controller
 {
@@ -17,35 +19,49 @@ class House extends Controller
     }
 
     /**
-     * 编辑(添加或修改)
+     * 添加
+     */
+    public function add(Request $request){
+        $result = input('post');
+        $data = array(
+            'address'   => '翻斗大街翻斗花园2号楼1001室',//$result['address']
+            'area'      => '280.5',//$result['area']
+            'doormodel' => '四室一厅二卫',//$result['doormodel']
+            'status'    => '2',//$result['status']
+        );
+
+        try{
+            //数据库存储操作
+            model('house')->save($data);
+            dump('成功');exit;
+        }catch (Exception $e){
+            return json(['data'=>'','status'=>400,'msg'=>'系统错误,请联系我们团队!']);
+        }
+    }
+
+    /**
+     * 编辑
      */
     public function edit(Request $request){
-        try{
-
-            if($request->isPost()){//若为post则添加
-                $result = input('post');
-                $data = array(
-                    'address'   => $result['address'],
-                    'area'      => $result['area'],
-                    'doormodel' => $result['doormodel'],
-                    'status'    => $result['status']
-                );
-                if($result['id'] == null){
-                    //添加
-                    //$this->add($data);
-                }else{
-                    //修改
-                    //$this->update($id,$data);
-                }
-
-            }else{//修改回显
-                $id = input('get')['id'];
-                //$this->select($id);
+        if($request->isPost()){
+            $result = input('post');//数据
+            $hid = $result['hid'];//主键
+            $data = array(
+                'address'   => $result['address'],
+                'area'      => $result['area'],
+                'doormodel' => $result['doormodel'],
+                'status'    => $result['status']
+            );
+            try{
+                model('house')->allowField(true)->save($data,['hid' => $hid]);
+                return json(['data'=>'','status'=>200,'msg'=>'用户编辑成功!']);
+            }catch (Exception $e){
+                return json(['data'=>'','status'=>400,'msg'=>'系统错误,请联系我们团队!']);
             }
+        }else{
 
-        }catch (Exception $e){
-            $this->error('编辑错误');
         }
+
 
 
     }
