554b976ceb7ec97959d32085ca88ace3d88acc6b
zufangke <602029937@qq.com>
2018-12-18 16:09:10 +0800

    更新权限，增加房东登录与注销

--- app/admin/controller/Base.php
+++ app/admin/controller/Base.php
@@ -5,19 +5,30 @@ use think\Controller;
 class Base extends Controller{
 
     function _initialize(){
+        //判断房东
         $adname = cookie('adname');
         $adid   = cookie('adid');
+        //判断普通租客
+        $phone = cookie('phone');
 
-        if (!isset($adid) && !isset($adname)){
+        if (!isset($adid) && !isset($phone)){
             header('location: http://'.$_SERVER['HTTP_HOST'].'Log/index');
             die;
         }
         // 登录用户进行再次验证
-        $where = array(
-            'adname' => ['=',$adname],
-            'adid' => ['=',$adid],
-        );
-        $adminArr = db('admin')->where($where)->select()[0];
+        if(empty($adname)){//若房东为空
+            $where = array(
+                'phone' => ['=',$phone],
+            );
+            $adminArr = db('user')->where($where)->select()[0];
+        }else{
+            $where = array(
+                'adname' => ['=',$adname],
+                'adid' => ['=',$adid],
+            );
+            $adminArr = db('admin')->where($where)->select()[0];
+        }
+
         if(empty($adminArr)){
             // 判断是否为合法数据
             header('location: http://'.$_SERVER['HTTP_HOST'].'index');
