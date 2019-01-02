cc9d7efab4377cfe1dc62ea06c8ac34a17e3f6df
zufangke <linchuantuo2016php@163.com>
2019-01-02 15:06:42 +0800

    增加adid

--- app/admin/controller/House.php
+++ app/admin/controller/House.php
@@ -14,38 +14,44 @@ use think\Request;
 class House extends Controller
 {
     //房源列表
-    public function index()
+    public function index(Request $request)
     {
-        $data = db('house')->select();
-        $nowTime = time();//当前时间
-        foreach ($data as $k => $v){
-            $difference = $nowTime-$v['update_time'];//时间差
-            if($v['status'] == 2 && $difference >= (60*60*24*30)){
-                $v['status'] = 4;//当同时满足两种条件时,标注为着火房(4代表着火房)
+        if ($request->isPost()){
+            $adid = input('post.adid');
+            $data = db('house')->where('adid',$adid)->select();
+            $nowTime = time();//当前时间
+            foreach ($data as $k => $v){
+                $difference = $nowTime-$v['update_time'];//时间差
+                if($v['status'] == 2 && $difference >= (60*60*24*30)){
+                    $v['status'] = 4;//当同时满足两种条件时,标注为着火房(4代表着火房)
+                }
+                $data[$k] = $v;
             }
-            $data[$k] = $v;
+            return $this->fetch('house_list',['data'=>$data]);
         }
-        return $this->fetch('house_list',['data'=>$data]);
+
     }
 
     /**
      * 添加
      */
     public function add(Request $request){
-        $result = input('post.');
-        $data = array(
-            'address'   => $result['address'],//$result['address']
-            'area'      => $result['area'],//$result['area']
-            'doormodel' => $result['doormodel'],//$result['doormodel']
-            'status'    => $result['status'],//$result['status']
-        );
-
-        try{
-            //数据库存储操作
-            model('house')->save($data);
-            return json(['data'=>'','status'=>200,'msg'=>'用户编辑成功!']);
-        }catch (Exception $e){
-            return json(['data'=>'','status'=>400,'msg'=>'系统错误,请联系我们团队!']);
+        if ($request->isPost()){
+            $result = input('post.');
+            $data = array(
+                'adid'      => $result['adid'],
+                'address'   => $result['address'],//$result['address']
+                'area'      => $result['area'],//$result['area']
+                'doormodel' => $result['doormodel'],//$result['doormodel']
+                'status'    => $result['status'],//$result['status']
+            );
+            try{
+                //数据库存储操作
+                model('house')->save($data);
+                return json(['data'=>'','status'=>200,'msg'=>'用户编辑成功!']);
+            }catch (Exception $e){
+                return json(['data'=>'','status'=>400,'msg'=>'系统错误,请联系我们团队!']);
+            }
         }
     }
 
