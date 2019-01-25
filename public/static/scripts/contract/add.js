bbf6a81548cd35949dbcad432a807e9ba0a661cf
quyueshi <quyueshi2@hotmail.com>
2019-01-25 17:06:18 +0800

    添加合同页面三级连查js优化

--- public/static/scripts/contract/add.js
+++ public/static/scripts/contract/add.js
@@ -9,57 +9,61 @@ $(function(){
         console.log(fileName);
     });
     //添加合同页面的小区选择事件
-    $(".house-id ul li").click(function(){
-        var $a = $(this).find("a");
-        var hid = $a.attr("data-hid");
-        var unit_ul = $(".unit-id ul");
-        unit_ul.html("");
-        $(".unit-id a input[type=hidden]").val();
-        $(".unit-id a input[type=hidden]").siblings("span").html("请选择楼栋");
-        $(".room-id ul").html("");
-        $(".room-id a input[type=hidden]").val();
-        $(".room-id a input[type=hidden]").siblings("span").html("请选择房间");
-        $.ajax({
-            url:"/contract/addpage",
-            data:{'hid':hid},
-            type:"post",
-            dataType:"json",
-            success:function (data){
-                if(data.status === 200){
-                    for(var i = 0; i < data.data.length; i++){
-                        //循环渲染对应小区下的楼栋单元
-                        unit_ul.append('<li role="presentation">' +
-                            '<a role="menuitem" tabindex="-1" href="javascript:void(0);" ' +
-                            'data-uid="'+data.data[i].roomid+'" value="'+data.data[i].room+'">'+data.data[i].room+'</a>' + '</li>');
-                    }
-                }
-            }
-        });
-    });
+    $(".house-id ul li").click(tree);
     //楼栋单元的点击事件
-    $(".unit-id ul").on("click","li",function(){
+    $(".unit-id ul").on("click","li",tree);
+    //房间或者楼栋选择点击事件，逐层显示下级
+    function tree(){
+        var isHouse = true;
+        var param = {};
         var $a = $(this).find("a");
-        var hid = $(".house-id ul li.active a").attr("data-hid");
-        var uid = $a.attr("data-uid");
-        var room_ul = $(".room-id ul");
-        room_ul.html("");
-        $(".room-id a input[type=hidden]").val();
-        $(".room-id a input[type=hidden]").siblings("span").html("请选择房间");
+        if($(this).parents(".dropdown-inner").hasClass("house-id")){
+            var unit_ul = $(".unit-id ul");
+            unit_ul.html("");
+            $(".unit-id a input[type=hidden]").val();
+            $(".unit-id a input[type=hidden]").siblings("span").html("请选择楼栋");
+            $(".room-id ul").html("");
+            $(".room-id a input[type=hidden]").val();
+            $(".room-id a input[type=hidden]").siblings("span").html("请选择房间");
+            param.hid = $a.attr("data-hid");
+        }else{
+            isHouse = false;
+            var room_ul = $(".room-id ul");
+            room_ul.html("");
+            $(".room-id a input[type=hidden]").val();
+            $(".room-id a input[type=hidden]").siblings("span").html("请选择房间");
+            param.roomid = $a.attr("data-uid");
+        }
         $.ajax({
             url:"/contract/addpage",
-            data:{'hid':hid,'uid':uid},
+            data:param,
             type:"post",
             dataType:"json",
             success:function (data){
                 if(data.status === 200){
-                    for(var i = 0; i < data.data.length; i++){
-                        //循环渲染对应小区下的楼栋单元下的房间列表
-                        room_ul.append('<li role="presentation">' +
-                            '<a role="menuitem" tabindex="-1" href="javascript:void(0);" ' +
-                            'data-uid="'+data.data[i].roomid+'" value="'+data.data[i].room+'">'+data.data[i].room+'</a>' + '</li>');
-                    }
+                    showLi(data.data,isHouse);
                 }
             }
         });
-    });
+    }
+    //渲染页面的下拉列表下级
+    function showLi(data,isHouse){
+        if(isHouse){
+            var unit_ul = $(".unit-id ul");
+            //循环渲染对应小区下的楼栋单元
+            for(var i = 0; i < data.length; i++) {
+                unit_ul.append('<li role="presentation">' +
+                    '<a role="menuitem" tabindex="-1" href="javascript:void(0);" ' +
+                    'data-uid="' + data[i].roomid + '" value="' + data[i].room + '">' + data[i].room + '</a>' + '</li>');
+            }
+        }else{
+            var room_ul = $(".room-id ul");
+            //循环渲染对应小区下的楼栋单元的房间列表
+            for(var i = 0; i < data.length; i++) {
+                room_ul.append('<li role="presentation">' +
+                    '<a role="menuitem" tabindex="-1" href="javascript:void(0);" ' +
+                    'data-roomid="' + data[i].underid + '" value="' + data[i].tag + '">' + data[i].tag + '</a>' + '</li>');
+            }
+        }
+    }
 });
