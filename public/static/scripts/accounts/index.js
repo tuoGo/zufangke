a05b8574868706f02337ad1ce6a80b5aa99a4a34
quyueshi <quyueshi2@hotmail.com>
2019-02-14 17:23:24 +0800

    js改动

--- public/static/scripts/accounts/index.js
+++ public/static/scripts/accounts/index.js
@@ -2,4 +2,17 @@ $(function(){
     $(".table-area .add .payoff").click(function(){
         $("#we-input").modal("show");
     });
+    $("#we-input").on("okHidden",function(){
+
+        // $.ajax({
+        //     url : "/accounts/init",
+        //     type : "post",
+        //     data : {},
+        //     success : function(data){
+        //         if(data.status === 200){
+        //
+        //         }
+        //     }
+        // });
+    });
 });
