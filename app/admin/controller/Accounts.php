095127dbc0e5fcb52eb4653e96059a2edd1fadab
zufangke <linchuantuo2016php@163.com>
2019-02-15 10:28:11 +0800

    修改报表模块

--- app/admin/controller/Accounts.php
+++ app/admin/controller/Accounts.php
@@ -20,43 +20,68 @@ class Accounts extends Base
      */
     public function index(){
         $adid    = Session::get('adid');//获取房东主键
-        $fcount  = db('financial')->where('adid',$adid)->count();
-        $list    = db('financial')->where('adid',$adid)->order('end_time desc')->paginate(14,$fcount);
-        $page    = $list->render();
-        $count   = db('underlying')->where('adid',$adid)->count();
-        $signing = db('underlying')->where('adid',$adid)->where('status != 0')->count();
-        $total   = db('financial')->where('adid',$adid)->sum('amount');
-        $delivery= db('financial')->where('adid',$adid)->sum('rent');
-        $costs   = db('financial')->where('adid',$adid)->sum('additional_costs');
-        foreach ($list as $key => $value){
-            $uid[$key] = $value['uid'];
-        }
-        $user = db('user')->where('uid','in',$uid)->select();
-        foreach ($list as $fk => $fv){
-            $flist[$fk] = $fv;
-            $flist[$fk]['start_time'] = date('Y年m月d日',$flist[$fk]['start_time']);
-            $flist[$fk]['end_time'] = date('Y年m月d日',$flist[$fk]['end_time']);
-            foreach ($user as $uk => $uv){
-                if ($flist[$fk]['uid'] == $uv['uid']){
-                    $flist[$fk]['user'] = $uv;
-                    continue;
+        $uid     = Session::get('uid');
+        if (!empty($adid)){
+            $fcount  = db('financial')->where('adid',$adid)->count();
+            $list    = db('financial')->where('adid',$adid)->order('end_time desc')->paginate(14,$fcount);
+            $page    = $list->render();
+            $count   = db('underlying')->where('adid',$adid)->count();
+            $signing = db('underlying')->where('adid',$adid)->where('status != 0')->count();
+            $total   = db('financial')->where('adid',$adid)->sum('amount');
+            $delivery= db('financial')->where('adid',$adid)->sum('rent');
+            $costs   = db('financial')->where('adid',$adid)->sum('additional_costs');
+            foreach ($list as $key => $value){
+                $uid[$key] = $value['uid'];
+            }
+            $user = db('user')->where('uid','in',$uid)->select();
+            foreach ($list as $fk => $fv){
+                $flist[$fk] = $fv;
+                $flist[$fk]['start_time'] = date('Y年m月d日',$flist[$fk]['start_time']);
+                $flist[$fk]['end_time'] = date('Y年m月d日',$flist[$fk]['end_time']);
+                foreach ($user as $uk => $uv){
+                    if ($flist[$fk]['uid'] == $uv['uid']){
+                        $flist[$fk]['user'] = $uv;
+                        continue;
+                    }
                 }
             }
+            $data = array(
+                'count'    => $count,   //房源总计
+                'signing'  => $signing, //已签约房源
+                'total'    => $total,   //总金额
+                'delivery' => $delivery,//租金收入
+                'costs'    => $costs,   //水电收入
+            );
+            return $this->fetch('index',['data'=>$data,'list'=>$flist,'page'=>$page,'user'=>'fangdong']);
+        }else if (!empty($uid)){
+            $fcount  = db('financial')->where('uid',$uid)->count();
+            $list    = db('financial')->where('uid',$uid)->order('end_time desc')->paginate(14,$fcount);
+            $page    = $list->render();
+            foreach ($list as $uk => $uv){
+                $flist[$uk] = $uv;
+                $flist[$uk]['start_time'] = date('Y年m月d日',$flist[$uk]['start_time']);
+                $flist[$uk]['end_time'] = date('Y年m月d日',$flist[$uk]['end_time']);
+            }
+            return $this->fetch('index',['list'=>$flist,'page'=>$page,'user'=>'zuke']);
         }
-        $data = array(
-            'count'    => $count,   //房源总计
-            'signing'  => $signing, //已签约房源
-            'total'    => $total,   //总金额
-            'delivery' => $delivery,//租金收入
-            'costs'    => $costs,   //水电收入
-        );
-        return $this->fetch('index',['data'=>$data,'list'=>$flist,'page'=>$page]);
     }
     /*
      * 向租客发起支付单
      */
-    public function init(){
-        $data = input('post.');
-        print_r($data);exit;
+    public function init(Request $request){
+        if ($request->isPost()){
+            $data = input('post.');
+            $contract = db('contract')->where('contid',$data['contid'])->find();
+            $rent  = db('financial')->field('rent')->where('fid',$data['fid'])->find()['rent'];
+            $water = $data['water'] * $contract['water'];
+            $elec  = $data['elec'] * $contract['elec'];
+            $costs = $water + $elec;
+            $sum   = $rent + $costs;
+            $rel   = db('financial')->where('fid',$data['fid'])->update(['water'=>$data['water'],'elec'=>$data['elec'],'additional_costs'=>$costs,'amount'=>$sum]);
+            if ($rel){
+                return json(['data'=>'','status'=>200,'msg'=>'已向租客发起支付!']);
+            }
+            return json(['data'=>'','status'=>400,'msg'=>'发起订单失败!']);
+        }
     }
 }
